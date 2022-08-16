bb217275aed8e8cd91d37d3b2abe83c761daeccc
jsdevraju <islamrajulearn.dev@gmail.com>
2022-08-16 08:49:34 +0600

    added scrollable food card

--- components/FeaturedRow/index.js
+++ components/FeaturedRow/index.js
@@ -1,15 +1,42 @@
 //import liraries
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from "react-native";
+import { ArrowRightIcon } from "react-native-heroicons/outline";
+import RestaurantCard from "../RestaurantCard";
 
 // create a component
-const FeaturedRow = () => {
-    return (
-        <View>
-            <Text>FeaturedRow</Text>
-        </View>
-    );
+const FeaturedRow = ({ id, title, description, featuredCategory }) => {
+  return (
+    <View>
+      <View className="flex-row mt-4 items-center justify-between px-4">
+        <Text className="font-bold text-lg">FeaturedRow</Text>
+        <ArrowRightIcon color="#00CCBB" />
+      </View>
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
+      <ScrollView
+        horizontal
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+        }}
+        showsHorizontalScrollIndicator={false}
+        className="pt-4"
+      >
+        {/* Restaurant Card */}
+        <RestaurantCard
+          id="1223"
+          imageUrl="https://links.papareact.com/gn7"
+          title="Yo pop"
+          rating={4.5}
+          genre-="Japanis"
+          address="123 Main Sr"
+          short_description="This is test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+      </ScrollView>
+    </View>
+  );
 };
-
 
 //make this component available to the app
 export default FeaturedRow;
---
+++ components/RestaurantCard/index.js
@@ -0,0 +1,47 @@
+//import liraries
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { LocationMarkerIcon } from "react-native-heroicons/outline";
+import { StarIcon } from 'react-native-heroicons/solid'
+
+// create a component
+const RestaurantCard = ({
+  id,
+  imageUrl,
+  title,
+  rating,
+  genre,
+  address,
+  short_description,
+  dishes,
+  long,
+  lat,
+}) => {
+  return (
+    <TouchableOpacity className="bg-white mr-3 shadow">
+      <Image
+        source={{
+          uri: imageUrl,
+        }}
+        className="h-36 w-64 rounded-sm"
+      />
+      <View className="px-3 pb-4">
+        <Text className="font-bold text-lg pt-2">{title}</Text>
+        {/* Rating */}
+        <View className="flex-row items-center space-x-1">
+            <StarIcon color="green" opacity={0.5} size={22} />
+            <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">{rating}</Text> {genre}
+            </Text>
+        </View>
+        {/* Location Marker */}
+        <View className="flex-row items-center space-x-1">
+            <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
+            <Text className="text-xs text-gray-600">Nearby {address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+//make this component available to the app
+export default RestaurantCard;

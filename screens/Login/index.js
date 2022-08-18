d09210174162a1e8218a141f23d2f011845b078c
jsdevraju <islamrajulearn.dev@gmail.com>
2022-08-18 11:24:02 +0600

    added redux

--- screens/Login/index.js
+++ screens/Login/index.js
@@ -4,24 +4,21 @@ import AppForm from "../../components/Form/AppForm";
 import AppFormFeilds from "../../components/Form/AppFormFeilds";
 import AppSubmitButton from "../../components/Form/AppSubmitButton";
 import { LoginSchema } from "../../Validation";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { login } from "../../utils";
 
-
 const Login = () => {
-
   const navigation = useNavigation();
 
   const loginUser = async (values) => {
     try {
       const res = await login(values);
-      console.log("res", res)
+      console.log("res", res);
       // await AsyncStorage.setItem('user', JSON.stringify(values));
     } catch (error) {
-      console.log("erro", error)
+      console.log("erro", error);
     }
-  }
-
+  };
 
   return (
     <SafeAreaView
@@ -30,13 +27,12 @@ const Login = () => {
       }}
       className="pt-5 flex-1 items-center justify-center"
     >
-
       <View className="shadow-md bg-white w-full p-8">
         <Text className="text-1xl font-bold text-center">
           Welcome to join our community
         </Text>
         <AppForm
-          initialValues={{email: "", password: "" }}
+          initialValues={{ email: "", password: "" }}
           validationSchema={LoginSchema}
           onSubmit={(values) => loginUser(values)}
         >
@@ -55,7 +51,12 @@ const Login = () => {
         </AppForm>
         <Text className="mt-4 text-gray-700">
           If you don't have account?
-          <Text className="text-blue-600" onPress={() => navigation.navigate("VerifyEmail")}>signup</Text>
+          <Text
+            className="text-blue-600"
+            onPress={() => navigation.navigate("VerifyEmail")}
+          >
+            signup
+          </Text>
         </Text>
       </View>
     </SafeAreaView>
